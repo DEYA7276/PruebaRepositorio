a64279afe611700af5487466a904b4b202d48d38
deyanira <23610030@utgz.edu.mx>
2024-11-03 13:45:44 -1200

    Primer commit del proyecto

---
+++ app/Http/Controllers/MiPrimerControlador.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MiPrimerControlador extends Controller
+{
+    public function index()
+    {
+        return "¡Hola, este es el método index de MiPrimerControlador!";
+    }
+}
---
+++ resources/views/Prueba.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Prueba View</title>
+</head>
+
+    <h1>Bienvenido a mi primera vista en Laravel :)</h1>
+   
+</body>
+</html>
